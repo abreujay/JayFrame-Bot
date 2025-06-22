3cdee4c0af32f38401c15d796fdc503915137dd1
Jonathan Abreu <jonathanvabreu26@gmail.com>
2025-06-22 17:03:48 -0300

    server updated

---
+++ routes/routes.js
@@ -0,0 +1,6 @@
+const {Router} = require('express')
+const router = Router()
+
+router.head('/health', (req, res) => {})
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,20 +1,14 @@
 const express = require("express");
-const axios = require("axios");
-const getItem = require("../warframe/wfService");
+const router = require("../routes/routes");
 
 const discBot = require('../discord/disBot')
-const { Client, GatewayIntentBits } = require("discord.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-
-  res.send('Bot Acordado!')
-
-})
+app.use(router)
 
 app.listen(PORT, () => {
 
